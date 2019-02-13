f376baa0aa2bc0624bbe8016d2dff4ed7611dc42
Alex M <amalki@piap.pl>
2019-02-13 13:20:58 +0100

    processing: fix processing issues

--- inc/processing.h
+++ inc/processing.h
@@ -5,15 +5,15 @@
 
 typedef struct processing_obj_st processing_obj;
 
-typedef int (*processing_data_in_cb	) (processing_obj *obj, message_obj *msg);
-typedef int (*processing_data_out_cb	) (processing_obj *obj, message_obj *msg);
+typedef size_t (*processing_data_in_cb	) (processing_obj * const obj, message_obj * const msg);
+typedef size_t (*processing_data_out_cb	) (processing_obj * const obj, message_obj * const msg);
 
 struct processing_obj_st {
 	processing_data_in_cb 		data_in;
 	processing_data_out_cb 		data_out;
 };
 
-int processing_init(processing_obj *obj);
-int processing_fini(processing_obj *obj);
+int processing_init(processing_obj * const obj);
+int processing_fini(processing_obj * const obj);
 
 #endif /* __PROCESSING_H__ */
