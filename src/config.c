6eef979abbaa10c2b61ecd500847f72e357a1a36
Alex M <amalki@piap.pl>
2019-02-13 09:02:05 +0100

    config: config now compile

--- src/config.c
+++ src/config.c
@@ -5,7 +5,6 @@
 
 typedef struct {
 	const char * const path_cfg;
-	const char * const path_cfg;
 
 	bool is_init;
 
@@ -14,23 +13,22 @@ typedef struct {
 
 static config_priv_data cfg_pdata;
 
-static int config_get_template(config_obj *str)
-{
-}
-
 int config_init(config_obj *obj)
 {
-	config_priv_data *pdata = (config_priv_data *)obj->pdata;
+	config_priv_data *pdata = &cfg_pdata; 
 
 	if (pdata->is_init) {
 		ERROR("configuration already initialized\n");
 		return -1;
 	}
 
+	obj->pdata = (void *) pdata;
+	pdata->is_init = true;
+	
 	return 0;
 }
 
-static config_obj * const config_get_instance(void)
+config_obj * const config_get_instance(void)
 {
 	return NULL;
 }
@@ -42,23 +40,23 @@ static cfg_param * const config_default_get_value(config_obj * const obj,
 	return NULL;
 }
 
-static void config_default_set_value(config_obj * const obj,
+static int config_default_set_value(config_obj * const obj,
 						cfg_param * const param)
 {
 	WARNING("The config module is not loaded\n");
-	return NULL;
+	return -1;
 }
 
 int config_fini(config_obj *obj)
 {
 	config_priv_data *pdata = (config_priv_data *)obj->pdata;
 
-	if (pdata->is_init)  {
+	if (!pdata->is_init)  {
 		WARNING("Configuration Already deinit\n");
 		return -1;
 	}
 	
-	pdata->is_init = 0;
+	pdata->is_init = false;
 	obj->get_value = config_default_get_value;
 	obj->set_value = config_default_set_value;
 
