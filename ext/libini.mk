adba32c2acc074865a414d0a2684b216d95fb7f2
Alexandre Malki <amalki@piap.pl>
2019-02-14 07:01:51 +0100

    recipe: added libini recipe to get INI library parser

--- ext/libini.mk
+++ ext/libini.mk
@@ -1,7 +1,7 @@
 # Basic recipe
 
 libini_git=https://github.com/rxi/ini.git
-libini_src_dir=./ini
+libini_src_dir=./libini
 libini_version=master
 
-libini_build_cmd="gcc -Wall -fPIC -c *.c && gcc -shared -Wl,-soname,libini.so.1 -o libini.so.1.0 *.o && ln -sf /opt/lib/libini.so.1.0 /opt/lib/libini.so"
+libini_build_cmd="gcc -Wall -fPIC -c ./src/ini.c && gcc -shared -Wl,-soname,libini.so.1 -o libini.so.1.0 ini.o && ln -sf libini.so.1.0 libini.so"
