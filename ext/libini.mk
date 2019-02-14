7f97a28b0572e00ad9fa2ef9b350a16644c92a31
Alexandre Malki <amalki@piap.pl>
2019-02-14 10:40:15 +0100

    recipes: updated recipes cjson and ini libraries

--- ext/libini.mk
+++ ext/libini.mk
@@ -4,4 +4,7 @@ libini_git=https://github.com/rxi/ini.git
 libini_src_dir=./libini
 libini_version=master
 
-libini_build_cmd="gcc -Wall -fPIC -c ./src/ini.c && gcc -shared -Wl,-soname,libini.so.1 -o libini.so.1.0 ini.o && ln -sf libini.so.1.0 libini.so"
+libini_need_autotoolize=1
+libini_source_files="src/ini.c"
+libini_build_cmd="autoreconf -i . && ./configure && make"
+
