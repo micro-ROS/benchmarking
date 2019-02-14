d1b20b6232995401d9d7f6211d4f3ba6b6879fd0
Alex M <amalki@piap.pl>
2019-02-14 07:37:38 +0100

    message: adding more constant in the headers

--- inc/message.h
+++ inc/message.h
@@ -9,18 +9,18 @@ typedef size_t (*message_write_cb	)
 		(message_obj * const obj, char * const buffer, size_t len);
 
 typedef size_t (*message_read_cb	)
-		(message_obj * const obj, char * const buffer);
+		(message_obj * const obj, char * const buffer, size_t len);
 
 typedef size_t (*message_length_cb	)
-		(message_obj * const obj);
+		(const message_obj * const obj);
 
 typedef size_t (*message_size_cb	)
-		(message_obj * const obj);
+		(const message_obj * const obj);
 
 typedef size_t (*message_cpy_cb )
 		(message_obj * const msg, message_obj * restrict src);
 
-typedef char *(*message_ptr_cb ) (message_obj * const obj);
+typedef char *(*message_ptr_cb ) (const message_obj * const obj);
 
 struct message_obj_st {
 	/** Read from a message's buffer */
