10831661148a6db4aa7518d5d33822d231dd6842
Alex M <amalki@piap.pl>
2019-02-21 14:32:59 +0100

    swd_ctrl: introducing swd control module to send SWD commands to the uC

---
+++ inc/swd_ctrl.h
@@ -0,0 +1,30 @@
+#ifndef __SWD_CTRL_H__
+#define __SWD_CTRL_H__
+
+typedef struct swd_ctrl_st swd_ctrl_obj;
+
+typedef int (*swd_ctrl_set_cfg_from_gbl_cfg_cb)
+				 (swd_ctrl_obj * const obj);
+typedef int (*swd_ctrl_set_cfg_cb)
+				 (swd_ctrl_obj * const obj, const char *name);
+typedef int (*swd_ctrl_start_cb)
+				(swd_ctrl_obj * const obj, char * const prog);
+typedef int (*swd_ctrl_stop_cb) (swd_ctrl_obj * const obj);
+
+struct swd_ctrl_st {
+	swd_ctrl_set_cfg_from_gbl_cfg_cb	set_cfg_if_from_gbl_cfg;
+	swd_ctrl_set_cfg_from_gbl_cfg_cb	set_cfg_cpu_from_gbl_cfg;
+	swd_ctrl_set_cfg_cb			set_cfg_if;
+	swd_ctrl_set_cfg_cb			set_cfg_cpu;
+
+	swd_ctrl_start_cb			start;
+	swd_ctrl_stop_cb			stop;
+
+	void 					*pdata;
+};
+
+
+int swd_ctrl_init(swd_ctrl_obj * const obj);
+int swd_ctrl_fini(swd_ctrl_obj * const obj);
+
+#endif /* __SWD_CTRL_H__ */
