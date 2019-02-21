ed9b76babf5b56138d4775e3160c025817eed917
Alexandre Malki <amalki@piap.pl>
2019-02-21 14:33:18 +0100

    openocd: adding openocd as a library

---
+++ ext/openocd.mk
@@ -0,0 +1,9 @@
+# Basic recipe
+
+openocd_git=https://github.com/ntfreak/openocd.git
+openocd_src_dir=./openocd
+openocd_version=v0.10.0
+
+openocd_build_cmd="./bootstrap && ./configure --disable-werror && make -j8"
+openocd_patches="0001-openocd_build_shared"
+
