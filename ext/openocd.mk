8fe0e9395ea6679f939038c4fb0bcce4d5149bbc
Alex M <amalki@piap.pl>
2019-03-04 11:32:39 +0100

    openocd: adding patch to create configuration

--- ext/openocd.mk
+++ ext/openocd.mk
@@ -5,5 +5,5 @@ openocd_src_dir=./openocd
 openocd_version=v0.10.0
 
 openocd_build_cmd="./bootstrap && ./configure --disable-werror && make -j8"
-openocd_patches="0001-openocd_build_shared"
+openocd_patches="0001-openocd_build_shared 0002-openocd-tcl-adding-stm32f407_execution-configuration.patch"
 
