d532f02511fbe4e94f727eb14394b0470483058d
Alex M <amalki@piap.pl>
2019-03-05 09:35:12 +0100

    pipeline: remote sink and attach element prototype changed

--- inc/pipeline.h
+++ inc/pipeline.h
@@ -10,11 +10,7 @@ typedef int (*pipeline_attach_src_cb)(pipeline_obj * const obj,
 				      processing_obj * const src);
 
 typedef int (*pipeline_attach_proc_cb)(pipeline_obj * const obj, 
-				       processing_obj * const src,
-	       			       processing_obj * const dst);
-
-typedef int (*pipeline_attach_sink_cb)(pipeline_obj * const obj, 
-				       processing_obj * const sink);
+				       processing_obj * const src);
 
 typedef int (*pipeline_stream_data_cb)(pipeline_obj *obj);
 typedef bool (*pipeline_is_stopped_cb)(pipeline_obj *obj);
@@ -22,7 +18,6 @@ typedef bool (*pipeline_is_stopped_cb)(pipeline_obj *obj);
 struct pipeline_obj_st {
 	pipeline_attach_src_cb   attach_src;
 	pipeline_attach_proc_cb  attach_proc;
-	pipeline_attach_sink_cb  attach_sink;
 	pipeline_stream_data_cb  stream_data;
 	pipeline_is_stopped_cb	 is_stopped;
 
