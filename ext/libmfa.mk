37acd8e204ab1133743453191e37d5a59d95061b
Alex M <amalki@piap.pl>
2019-04-09 09:28:18 +0200

    mfa: adding external library to analyze memory

---
+++ ext/libmfa.mk
@@ -0,0 +1,7 @@
+# Basic recipe
+
+libmfa_git=git@gitlab.com:micro-ROS/memory_footprint_analyzer.git
+libmfa_src_dir=./libmfa
+libmfa_version=master
+
+libmfa_build_cmd="./autogen.sh && ./configure && make"
